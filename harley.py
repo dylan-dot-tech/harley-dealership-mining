9324f927f75392b89d85f83120d2dc6783b0d97b
dylan.tech <hi@dylan.tech>
2022-11-03 15:16:09 -0400

    PoC, pulling info from search page

--- harley.py
+++ harley.py
@@ -4,7 +4,7 @@
 @author: dylan.tech
 @contact: hi@dylan.tech
 @copyright: https://dylan.tech
-@version: 2022-11-02
+@version: 2022-11-03
 """
 
 from selenium import webdriver
@@ -17,24 +17,54 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 BASE_URL = 'https://www.harley-davidson.com/us/en/tools/find-a-dealer.html'
-driver = webdriver.Firefox(service=Service(executable_path=GeckoDriverManager().install()))
-driver.get(BASE_URL)
+zip_code = 49426    # Grand Rapids, MI
+# zip_code = 99782    # Wainwright, AK - no results example
 
-zip_code = 49417
+try:
+	# Init webdriver
+	driver = webdriver.Firefox(service=Service(
+		executable_path=GeckoDriverManager().install()))
+	driver.get(BASE_URL)
 
-enter_address = driver.find_element('id', 'find-term')
-enter_address.clear()
-enter_address.send_keys(zip_code)
+	# Select 'Enter Zip, Address, or Dealer Name' field
+	enter_zip = driver.find_element('id', 'find-term')
+	# Fill zip code field
+	enter_zip.clear()
+	enter_zip.send_keys(zip_code)
 
-# Click 'Find' button, doesn't appear until after the zip_code was entered
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'find__form__submit__button'))).click()
+	# Wait for 'Search' button to load after zip code field is filled, click
+	# when available
+	WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+		(By.CLASS_NAME, 'find__form__submit__button'))).click()
 
-# Get number of dealers near zip_code (caps at 50)
-num_results = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'find__results__head__count'))).text
-print(num_results)
-# Get number of search result pages
-results_pagination = driver.find_element(By.CLASS_NAME, 'find__results__pagination__page__total').text
+	# Get number of dealers near zip code, caps at 50
+	num_results = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'find__results__head__count')))
+	# Example num_results >> "50 dealers near Hudsonville, MI 49426, USA"
+	num_dealers = int(num_results.text.split()[0])	# Get number of dealers from above string
+	print("Number of dealers: {}".format(num_dealers))
 
-# Done
-driver.close()
-print('Done')
+	# Wait for search results to load, TODO: if no results, exception is thrown
+	WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+		(By.CLASS_NAME, 'find__results__list')))
+
+	# Get number of search result pages
+	results_pagination = driver.find_element(By.CLASS_NAME, 'find__results__pagination__page__total').text
+	print('Results pagination total: {}'.format(results_pagination))
+
+	# Get list of dealers from current page
+	dealer_list = driver.find_element(By.CLASS_NAME, 'find__results__list')
+	dealers = dealer_list.find_elements(By.TAG_NAME, 'li')
+
+	# For each dealer listed on current page
+	for dealer in dealers:
+		text = dealer.text
+		print(text)
+		print('---')
+
+except Exception as err:
+	print("Exception!")
+	print(err)
+
+finally:
+	driver.close()
+	print('Done')
